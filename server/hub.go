f962bc2724d4cb924388cf7be6ba99319fd91d86
HarryCoburn <harryjcoburn@gmail.com>
2026-08-16 10:12:00 -0400

    Refined hub for clarity. Added closeClient(). Moved clientList into the struct.

--- server/hub.go
+++ server/hub.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-// Chat hub
 type Hub struct {
-	Register   chan *Client
-	Unregister chan *Client
-	Broadcast  chan Message
-	Query      chan ClientNumReq
-	Done       chan struct{} // Signal we're done with the hub. Begin teardown.
-	Finished   chan struct{} // Signal the hub is completely closed.
+	Register   chan *Client         // Register a new client
+	Unregister chan *Client         // Unregister a client
+	Broadcast  chan Message         // Send a message to all clients
+	Query      chan ClientNumReq    // Send a query to the server
+	Done       chan struct{}        // Signal we're done with the hub. Begin teardown.
+	Finished   chan struct{}        // Signal the hub is completely closed.
+	clientList map[*Client]struct{} // Map of all clients.
 }
 
 type Message struct {
-	From *Client
-	Data []byte
+	From *Client // Address of the client that sent the message
+	Data []byte  // Content of the message
 }
 
 // Create a new hub
@@ -26,42 +26,43 @@ func newHub() *Hub {
 		Query:      make(chan ClientNumReq),
 		Done:       make(chan struct{}),
 		Finished:   make(chan struct{}),
+		clientList: make(map[*Client]struct{}),
 	}
 	return &hub
 }
 
-// The channels for the chat are processed here, as is the clientList
+const messageFormat = "<%s> %s"
+
+// Start the hub
 func (h *Hub) run() {
 	defer close(h.Finished)
-	clientList := make(map[*Client]struct{})
+
 	for {
 		select {
 		case c := <-h.Register:
 			fmt.Println("Received a registration request.")
-			clientList[c] = struct{}{}
+			h.clientList[c] = struct{}{}
 		case c := <-h.Unregister:
 			fmt.Println("Received an unregistration request")
-			if _, ok := clientList[c]; !ok { // Already removed, possibly by the drop path
-				continue
-			}
-			close(c.Out) // Close writer loop
-			delete(clientList, c)
+			h.closeClient(c)
 		case msg := <-h.Broadcast:
-			msg.Data = fmt.Appendf(nil, "<%s> %s", msg.From.Name, msg.Data)
-			for client := range clientList {
+			msg.Data = fmt.Appendf(nil, messageFormat, msg.From.Name, msg.Data)
+			// Fanout. TODO, a field in Message to know to do a private send?
+			for c := range h.clientList {
 				select {
-				case client.Out <- msg:
-				default: // Client has a problem. Start closing the client.
-					close(client.Out)
-					delete(clientList, client)
+				case c.Out <- msg:
+				default: // If a c.Out cannot be reached, assume the client has left and close it.
+					h.closeClient(c)
 				}
 			}
 		case req := <-h.Query:
-			clientNum := len(clientList)
+			// Currenly only handles clientList length requests
+			clientNum := len(h.clientList)
 			req.reply <- clientNum
-		case <-h.Done: // For safe teardown
-			for c := range clientList {
-				close(c.Out)
+		case <-h.Done:
+			// Start teardown by closing all clients. h.Finished will signal the rest.
+			for c := range h.clientList {
+				h.closeClient(c)
 			}
 			return
 		}
@@ -72,4 +73,12 @@ func (h *Hub) clientCount() int {
 	ch := make(chan int, 1)
 	h.Query <- ClientNumReq{reply: ch}
 	return <-ch
+}
+
+func (h *Hub) closeClient(c *Client) {
+	if _, ok := h.clientList[c]; !ok {
+		return
+	}
+	close(c.Out)
+	delete(h.clientList, c)
 }
